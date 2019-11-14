1a4b9c8387dbe201818728a3ed95242d4dfa4d77
Emaisty <maxgusevfff@yandex.ru>
2019-11-15 00:32:10 +0300

    fix bug with gitignore

--- lib/src/screenshot/image.go
+++ lib/src/screenshot/image.go
@@ -146,6 +146,7 @@ static void ImageToRGBAWindows(unsigned char *data, int32_t length, unsigned cha
 }
 
 static void ImageToRGBALinux(unsigned char *data, int32_t length) {
+	unsigned char t;
 	for (int32_t i = 0; i < length; i += 4) {
 		data[i] = data[i+2];
 		data[i+2] = data[i];
